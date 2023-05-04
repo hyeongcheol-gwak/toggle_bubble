35c86633f7f1aa0aed5dd8776c334f75e09c51c6
hyeongcheol-gwak <gwak0720@snu.ac.kr>
2023-05-04 21:27:29 +0900

    bubble

--- logger.js
+++ logger.js
@@ -66,7 +66,9 @@ logger.add(
   new winston.transports.Console({
     format: winston.format.combine(
       winston.format.colorize(), // 색깔 넣어서 출력
-      winston.format.simple() // `${info.level}: ${info.message} JSON.stringify({ ...rest })` 포맷으로 출력
+      winston.format.printf(({ level, message, label, timestamp }) => {
+        return `${convertToKST(timestamp)} [${label}] ${level}: ${message}`; // 날짜 [시스템이름] 로그레벨 메세지
+      })
     ),
   })
 );
